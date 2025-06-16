b87d944dbfb2e4c290b8729005c1dfd818375a91
slowstate <slowstatedev@gmail.com>
2025-06-17 09:33:06 +1000

    Remove _on_area_entered overrides in Lizard head, Oracle orbs, and Star shells

--- Enemies/Oracle/Orb/orb.gd
+++ Enemies/Oracle/Orb/orb.gd
@@ -14,12 +14,3 @@ func _ready() -> void:
 	set_collision_mask_value(Globals.CollisionLayer.BULLETS, true)
 	self.area_entered.connect(_on_area_entered)
 	set_colour(oracle.orb_colour)
-
-
-func _on_area_entered(area: Area2D) -> void:
-	var bullet = area as Bullet
-	if bullet.colour != oracle.orb_colour:
-		SfxManager.play_sound("EnemyDeflectSFX", -5.0, -3.0, 0.95, 1.05)
-		return
-	SfxManager.play_sound("EnemyHitSFX", -25.0,-23.0,2.0,2.2)
-	queue_free()
