83cd2125af9ed346af70bee1fc7f790969672e79
slowstate <slowstatedev@gmail.com>
2025-08-05 22:54:52 +1000

    Bot balance

--- Enemies/Bot/bot_state_aggro.gd
+++ Enemies/Bot/bot_state_aggro.gd
@@ -2,7 +2,7 @@ class_name BotStateAggro
 extends State
 
 var bot: Bot
-var aggro_time: float = 3.0
+var aggro_time: float = 2.0
 var aggro_cooldown_time: float = 3.0
 var target_location: Vector2
 
@@ -51,13 +51,13 @@ func physics_update(delta: float) -> void:
 		bot.move_forward(
 			delta,
 			Globals.player.global_position,
-			lerpf(bot.move_speed, bot.move_speed * 2.75, ease(1 - aggro_timer.time_left / aggro_timer.wait_time, -2.0) * 2.0)
+			lerpf(bot.move_speed, bot.move_speed * 2.5, ease(1 - aggro_timer.time_left / aggro_timer.wait_time, -2.0) * 2.0)
 		)
 	if !aggro_2_timer.is_stopped():
 		bot.move_forward(
 			delta,
 			Globals.player.global_position,
-			lerpf(bot.move_speed * 2.75, bot.move_speed * 0.75, ease(1 - aggro_2_timer.time_left / aggro_2_timer.wait_time, 2.0))
+			lerpf(bot.move_speed * 2.5, bot.move_speed * 0.75, ease(1 - aggro_2_timer.time_left / aggro_2_timer.wait_time, 2.0))
 		)
 
 	bot.set_stun_indicator_percentage_completion(1 - aggro_cooldown_timer.time_left / aggro_cooldown_timer.wait_time)
