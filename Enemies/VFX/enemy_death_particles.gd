e5a6ca979f10bed64e55e7d94417475185651422
slowstate <slowstatedev@gmail.com>
2025-08-07 18:10:33 +1000

    Add arms to bot death particles

--- Enemies/VFX/enemy_death_particles.gd
+++ Enemies/VFX/enemy_death_particles.gd
@@ -4,6 +4,8 @@ extends GPUParticles2D
 var sprite_global_rotation: float
 
 @onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
+@onready var gpu_particles_2d_2: GPUParticles2D = $GPUParticles2D2
+@onready var gpu_particles_2d_3: GPUParticles2D = $GPUParticles2D3
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 
@@ -19,6 +21,8 @@ func set_sprite_global_rotation(global_rotation: float) -> void:
 func _physics_process(delta: float) -> void:
 	if emitting:
 		gpu_particles_2d.emitting = true
+		gpu_particles_2d_2.emitting = true
+		gpu_particles_2d_3.emitting = true
 		sprite_2d.modulate.a = lerp(sprite_2d.modulate.a, 0.0, 15.0 * delta)
 
 
