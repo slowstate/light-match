ffe95a8802ea3cd20605d7ad930d084dbba5c939
slowstate <slowstatedev@gmail.com>
2025-08-13 18:14:23 +1000

    Fix typo

--- Menus/MainMenu/lifetime_palettes_label.gd
+++ Menus/MainMenu/lifetime_palettes_label.gd
@@ -5,7 +5,7 @@ extends Label
 func _ready() -> void:
 	var text_string = """Lifetime Chains: {lifetime_palettes}
 	Permanent fire rate gained: {fire_rate}%
-	Permanent max gealth gained: {max_health}"""
+	Permanent max health gained: {max_health}"""
 	var fire_rate = Save.lifetime_palettes * 0.002 * 100
 	var max_health = floori(Save.lifetime_palettes / 100)
 	text = text_string.format({"lifetime_palettes": Save.lifetime_palettes, "fire_rate": fire_rate, "max_health": max_health})
